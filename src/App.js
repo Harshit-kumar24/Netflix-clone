988151d255dfbe82f6cb2ee6485654230cc7a949
Harshit Kumar <harshitkumar9548@gmail.com>
2023-02-04 19:04:32 +0530

    login & register page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './app.scss';
 import Home from "../src/pages/home/Home";
 import Watch from './pages/watch/Watch';
+import Register from './pages/register/Register';
+import Login from './pages/login/Login';
 
 function App() {
   return (
     <div className="App">
-       <Home/>
+       <Watch/>
     </div>
   );
 }
